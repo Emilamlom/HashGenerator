b0429fea79c7b8ae214f24afcb18116f669bd09b
Emily K <104171497+Emilamlom@users.noreply.github.com>
2022-05-13 18:55:05 -0500

    Working on Hashes class, Added test wordlists

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from hashes import *
 
 class GUI:
@@ -13,7 +14,7 @@ class GUI:
         opt.set(options[0])
 
         self.input_frame = Frame(self.window)
-        self.input_frame.grid(column=0, row=0, columnspan=2)
+        self.input_frame.grid(column=0, row=0, columnspan=3)
         self.wordlist1_label = Label(self.input_frame, text='1st Wordlist:', padx=5, pady=5).grid(column=0, row=0)
         self.wordlist1_entry = Entry(self.input_frame, width=30)
         self.wordlist2_label = Label(self.input_frame, text='2nd Wordlist:', padx=5, pady=5).grid(column=0, row=1)
@@ -28,12 +29,13 @@ class GUI:
         self.algo_entry.grid(column=1, row=3)
 
         self.button_frame = Frame(self.window)
-        self.button_frame.grid(column=0, row=1, columnspan=2)
+        self.button_frame.grid(column=0, row=1, columnspan=3)
         self.button_test = Button(self.button_frame, text='Test', command=self.testbutton, padx=5, pady=5, width=10).grid(column=0, row=0)
         self.button_compute = Button(self.button_frame, text='Compute', command=self.computebutton, padx=5, pady=5, width=10).grid(column=1, row=0)
+        self.button_help = Button(self.button_frame, text='Help', command=self.helpbutton, padx=5, pady=5, width=10).grid(column=2, row=0)
 
         self.result_frame = Frame(self.window)
-        self.result_frame.grid(column=0, row=2, columnspan=2)
+        self.result_frame.grid(column=0, row=2, columnspan=3)
 
         self.window.columnconfigure(0, weight=1)
         self.window.columnconfigure(1, weight=1)
@@ -41,6 +43,17 @@ class GUI:
         self.window.columnconfigure(3, weight=1)
         self.window.rowconfigure(1, weight=1)
 
+    def helpbutton(self) -> None:
+        """
+        Displays messagebox with help information.
+        """
+        messagebox.showinfo('Explanation',
+                            "Wordlists and suffix are combined and hashed with the selected algorithm.\n\n"
+                            "Wordlist1 and Wordlist2: input the filepath of a wordlist.\n"
+                            "Wordlists should be in 'wordlists' directory for easiest access.\n"
+                            "Wordlists should be text files with each word separated by linebreaks.\n"
+                            "Suffix is for adding an optional suffix to each wordlist combo.\n")
+        
     def testbutton(self) -> None:
         """
         Runs a single instance of the hash function to check output and handles display of results
@@ -49,10 +62,11 @@ class GUI:
         if hasattr(self, 'label_result'):
             self.label_result.config(text='')
 
-        result = Hashes.hashtest(self.wordlist1_entry.get(), self.wordlist2_entry.get(), self.suffix_entry.get())
+        result = Hashes.hashtest(self.wordlist1_entry.get(), self.wordlist2_entry.get(), self.suffix_entry.get(), self.algo_entry.get())
         self.wordlist1_entry.delete(0, 'end')
         self.wordlist2_entry.delete(0, 'end')
         self.suffix_entry.delete(0, 'end')
+        self.algo_entry.set(options[0])
 
         self.label_result = Label(self.result_frame, text=result, padx=5, pady=5)
         self.label_result.grid(column=0, row=0)
@@ -65,10 +79,11 @@ class GUI:
         if hasattr(self, 'label_result'):
             self.label_result.config(text='')
 
-        result = Hashes.hashbatch(self.wordlist1_entry.get(), self.wordlist2_entry.get(), self.suffix_entry.get())
+        result = Hashes.hashbatch(self.wordlist1_entry.get(), self.wordlist2_entry.get(), self.suffix_entry.get(), self.algo_entry.get())
         self.wordlist1_entry.delete(0, 'end')
         self.wordlist2_entry.delete(0, 'end')
         self.suffix_entry.delete(0, 'end')
+        self.algo_entry.set(options[0])
 
         self.label_result = Label(self.result_frame, text=result, padx=5, pady=5)
         self.label_result.grid(column=0, row=0)
--- hashes.py
+++ hashes.py
@@ -1,15 +1,31 @@
+import hashlib
+
 class Hashes:
-    def hash(radius):
+    def hash(text, opt):
         '''
-        Function to compute the area of a circle.
-        :param radius: Radius of the circle
-        :return: the circle's area
+        Computes the hash of a given function using the chosen algorithm
+        :param text: A string of text
+        :param opt: str; should only be md5, sha256, or sha512
+        :return: the computed hash
         '''
+        if opt == 'md5':
+            return hashlib.md5(text).hexdigest()
+        elif opt == 'sha256':
+            return hashlib.sha256(text).hexdigest()
+        elif opt == 'sha512':
+            return hashlib.sha512(text).hexdigest()
+        else:
+            raise ValueError(f'{opt} is not a valid hashtype')
+
+    def hashtest(wordlist1, wordlist2, suffix, option):
+        with open(wordlist1, 'r') as w, open(wordlist2, 'r') as f:
+            word1 = w.readline().rstrip()
+            word2 = f.readline().rstrip()
+            text = word1 + word2 + suffix
         try:
-            radius = float(radius)
-            if radius <= 0:
-                raise ValueError('Values for area cannot be negative')
-            else:
-                return (radius ** 2) * pi
+            return Hashes.hash(text, option)
         except ValueError as e:
             return e
+
+    def hashbatch(self):
+        pass
